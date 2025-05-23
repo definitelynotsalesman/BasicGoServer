1f6f7f64b4c74e5fce269fa0536e464cb42840aa
definitelynotkevin <definitelynotkevincs@gmail.com>
2025-05-23 04:21:44 -0500

    testing broadcast logic

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "sync"
+
+    "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true // allow all connections
+    },
+}
+
+var (
+    clients = make(map[*websocket.Conn]bool)
+    mutex   = sync.Mutex{}
+)
+
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("New connection attempt...")
+    fmt.Println("New connection request...")
+
     conn, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
         fmt.Println("Upgrade error:", err)
@@ -22,7 +45,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
         fmt.Println("Received:", string(msg))
 
-        // Broadcast to all other connected clients
+        // Broadcast to all other clients
         mutex.Lock()
         for client := range clients {
             if client != conn {
@@ -42,4 +65,26 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
     delete(clients, conn)
     mutex.Unlock()
     fmt.Println("Client disconnected:", conn.RemoteAddr())
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "WebSocket server is running.")
+}
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "10000"
+    }
+
+    http.HandleFunc("/", handleRoot)
+    http.HandleFunc("/ws", handleWebSocket)
+
+    addr := "0.0.0.0:" + port
+    fmt.Println("Listening on", addr)
+
+    if err := http.ListenAndServe(addr, nil); err != nil {
+        fmt.Println("Server error:", err)
+        os.Exit(1)
+    }
 }
